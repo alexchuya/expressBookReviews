c30e7d23306b059f61e1c61a0886a19f908c5dff
alexchuya <alexfernand_17@hotmail.com>
2024-02-28 03:24:37 -0500

    implementacion de solicitudes HTTP

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -10,21 +10,63 @@ const isValid = (username)=>{ //returns boolean
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
-}
+    let validusers = users.filter((user)=>{
+        return (user.username === username && user.password === password)
+      });
+      if(validusers.length > 0){
+        return true;
+      } else {
+        return false;
+      }
+    }
+
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+    const username = req.body.username;
+    const password = req.body.password;
+    if (!username || !password) {
+        return res.status(404).json({message: "Error logging in"});
+    }
+    if (authenticatedUser(username,password)) {
+      let accessToken = jwt.sign({
+        data: password
+      }, 'access', { expiresIn: 60 * 60 });
+      req.session.authorization = {
+        accessToken,username
+    }
+    return res.status(200).send("User successfully logged in");
+    } else {
+      return res.status(208).json({message: "Invalid Login. Check username and password"});
+    }
+  });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let review = req.query.review;
+    const username = req.body.username;
+    const isbn = req.params.isbn;
+
+    let reviewBook = Object.values(books[parseInt(isbn)]); 
+    reviewBook[2][username] = review
+     res.send(reviewBook)
+
+
 });
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const username = req.body.username;
+    const isbn = req.params.isbn;
+
+    let reviewBook = Object.values(books[parseInt(isbn)]); 
+    let review = Object.values(reviewBook[2])
+
+     deletedBooks = review.filter((review) => review.username != username);
+     res.send( ` ${username} review deleted.`);
+    });
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
+
+
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,41 +3,104 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+const axios = require('axios');
 
+public_users.post('/register', (req,res) => {
 
-public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const doesExist = (username)=>{
+        let userswithsamename = users.filter((user)=>{
+          return user.username === username
+        });
+        if(userswithsamename.length > 0){
+          return true;
+        } else {
+          return false;
+        }
+      }
+      
+    const username = req.body.username;
+    const password = req.body.password;
+    if (username && password) {
+      if (!doesExist(username)) { 
+        users.push({"username":username,"password":password});
+        return res.status(200).json({message: "User successfully registred. Now you can login"});
+      } else {
+        return res.status(404).json({message: "User already exists!"});    
+      }
+    } 
+    return res.status(404).json({message: "Unable to register user."});
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    res.send(JSON.stringify({books},null,4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    res.send( books[isbn])
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+        const author = req.params.author;
+        const lenBooks = Object.keys(books);
+        for (var i = 1; i < lenBooks.length; i++) {
+            if (books[i].author===author){
+                res.send( books[i])
+            }
+          }
+        
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const title = req.params.title;
+    const lenBooks = Object.keys(books);
+    for (var i = 1; i < lenBooks.length; i++) {
+        if (books[i].title===title){
+            res.send( books[i])
+        }
+      }
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    const reviewBook= Object.values(books[parseInt(isbn)])
+    //console.log(reviewBook[2])
+    res.send(reviewBook[2])
 });
 
+
+
 module.exports.general = public_users;
+
+
+//  get list of books using async-await with axios
+
+axios.get("https://alexfernand2-5000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/")
+.then((response) => console.log("Lista de libros",response.data))
+.catch((error) => console.log(error));
+
+//  getting the book details based on ISBN using  axios 
+
+axios.get("https://alexfernand2-5000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/isbn/2")
+.then((response) => console.log(response.data))
+.catch((error) => console.log(error));
+
+//  getting the book details based on Author using  axios 
+
+axios.get("https://alexfernand2-5000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/author/Hans Christian Andersen")
+.then((response) => console.log(response.data))
+.catch((error) => console.log(error));
+
+//  getting the book details based on title using  axios 
+
+axios.get("https://alexfernand2-5000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/title/Things Fall Apart")
+.then((response) => console.log(response.data))
+.catch((error) => console.log(error));
+
+
+
